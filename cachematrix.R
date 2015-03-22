6fcd528082456d599ba19978e997c913c1a6416f
Manuel F. Bertoa <bertoa@lcc.uma.es>
2015-03-22 14:23:39 +0100

    two functions first try

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,32 @@
 ## Assigment 2 by Manuel F. Bertoa.
 ## Put comments here that give an overall description of what your
 ## functions do
+## Matrix inversion, caching the results so no repited inversions 
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function(){
+        return(x)
+    }
+    setinverse <- function(solve){
+        m <<- solve
+    }
+    getinverse <- function(){
+        return(m)
+    }
+    list(set = set, get = get,
+         setinverse = setinverse, getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
-## Return a inverse matix of x
+## Return a inverse matrix of x
 ## if inverse was cached, return the cached inverse matrix
 
 cacheSolve <- function(x, ...) {
@@ -20,7 +36,7 @@ cacheSolve <- function(x, ...) {
         return(m)
     }
     data <- x$get
-    m <- inverse(data, ...)
+    m <- solve(data, ...)
     x$setinverse(m)
     
     ## Return a matrix that is the inverse of 'x'
